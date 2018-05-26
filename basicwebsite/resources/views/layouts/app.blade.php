65bae70072513700eea367b4c4bdac7f72497183
hoodielive <hoodielive@gmail.com>
2018-05-26 10:29:51 -0400

    basic ws with laravel - created blades for app-home-about-contact

---
+++ basicwebsite/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ACME</title>
+</head>
+<body>
+    @yield('content')
+
+    @section('sidebar')
+        <div class="sidebar">
+            <h3>Sidebar</h3>
+            This is the sidebar
+        </div>
+</body>
+</html>
