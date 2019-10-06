e080049fbd267f5c0f10e6b548c05b5b8ff8d1f0
hoodielive <hoodielive@gmail.com>
2019-10-05 22:09:10 -0400

    form

---
+++ fundamentals/database/connection.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Connection 
+{
+    public function make()
+    {
+        try 
+        {
+            return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'laradev', 'password');
+        } catch(PDOException $e)
+        {
+            die($e->getMessage());
+        }
+    }
+}
+
+$pdo = Connection::make(); 
