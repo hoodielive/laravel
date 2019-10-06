6bb75f971f27b298e67654f02311697718c02ac9
hoodielive <hoodielive@gmail.com>
2019-10-05 20:49:24 -0400

    this freakin thing is not working

--- fundamentals/pdo_magus.php
+++ fundamentals/pdo_magus.php
@@ -1,11 +1,12 @@
 <?php 
 
-try {
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'laradev', '*07AF1DA1167810656EC10D04870D5E34E01316CB');
+// try {
 
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'laradev', '*07AF1DA1167810656EC10D04870D5E34E01316CB');
-} catch (PDOException $e) {
-    die('Could not connect.');
-}
+//     $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'laradev', '*07AF1DA1167810656EC10D04870D5E34E01316CB');
+// } catch (PDOException $e) {
+//     die('Could not connect.');
+// }
 
 $statement = $pdo->prepare('select * from todos');
 
