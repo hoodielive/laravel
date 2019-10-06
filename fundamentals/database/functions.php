524c945e8dcae25b1c9c382ad82a5496d7774c15
hoodielive <hoodielive@gmail.com>
2019-10-06 18:16:45 -0400

    teaching..

--- fundamentals/database/functions.php
+++ fundamentals/database/functions.php
@@ -1,1 +1,18 @@
 <?php 
+
+
+
+function fetchAllTasks($pdo)
+{
+    $statement = $pdo->prepare('select * from todos');
+
+    $statement->execute(); 
+
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+}
+
+function dd($data)
+{
+    echo 'pre'; 
+    die(var_dump($data));
+}
